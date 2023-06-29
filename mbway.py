b8c7610cd1c360cb8474e1a5cf91238151134b0e
bs <bfalmeidasilva@tecnico.ulisboa.pt>
2023-06-30 00:37:34 +0100

    Improved mbway message format

--- mbway.py
+++ mbway.py
@@ -2,8 +2,8 @@ from datetime import datetime
 import pandas as pd
 
 
-def entry_to_string(data: datetime) -> str:
-    return f"{data.day:02}/{data.month:02} às {data.hour:02}h{data.minute:02}"
+def entry_to_string(data: datetime, valor: int, parceiro: str) -> str:
+    return f"{data.hour:02}h{data.minute:02} {valor}€ {parceiro}\n"
 
 
 def mbway(data):
@@ -20,18 +20,29 @@ def mbway(data):
     premio_mensal = tabela_premios.loc[tabela_premios.tipo ==
                                        "Prémio Mensal"].loc[data:].head(1)
 
-    premio_diario_str = ""
-    for data_indice in premio_diario.index:
-        premio_diario_str += f"Diário:  " + entry_to_string(data_indice)
-        premio_diario_str += f" ({int(premio_diario.loc[data_indice].valor)}€) "
-        premio_diario_str += f"{premio_diario.loc[data_indice].parceiro} \n"
+    premio_diario_str = f"Dia {data.day:02}/{data.month:02}:\n"
+
+    for data_indice, premio in premio_diario.iterrows():
+        premio_diario_str += entry_to_string(data_indice, int(premio.valor), premio.parceiro)
+        #premio_diario_str += f"Diário:  " + entry_to_string(data_indice)
+        #premio_diario_str += f" {int(premio_diario.loc[data_indice].valor):3}€ "
+        #premio_diario_str += f"{premio_diario.loc[data_indice].parceiro} \n"
+
 
     data_semanal = premio_semanal.index[0]
     data_mensal = premio_mensal.index[0]
 
-    return premio_diario_str + \
-    f"Semanal: " + entry_to_string(data_semanal) + f" (100€) \n"\
-    f"Mensal:  " + entry_to_string(data_mensal) + f" (200€)"
+    premio_semanal_str = f"Semanal: {data_semanal.day:02}/{data_semanal.month:02}, "
+    premio_semanal_str += entry_to_string(data_semanal,
+            premio_semanal.loc[data_semanal].valor,
+            premio_semanal.loc[data_semanal].parceiro)
+
+    premio_mensal_str = f"Mensal:  {data_mensal.day:02}/{data_mensal.month:02}, "
+    premio_mensal_str += entry_to_string(data_mensal,
+            premio_mensal.loc[data_mensal].valor,
+            premio_mensal.loc[data_mensal].parceiro)
+
+    return premio_diario_str + "\n" + premio_semanal_str + premio_mensal_str
 
 def fetch_prize_table():
     dataframes_mbway = pd.read_html("https://www.mbway.pt/challenge-regulamento/")
