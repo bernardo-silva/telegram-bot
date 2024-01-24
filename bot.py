ff775e09273d640c9256a0449763ba640d47ff43
Bernardo Silva <bfalmeidasilva@tecnico.ulisboa.pt>
2024-01-24 22:06:21 +0000

    Update tabela

--- bot.py
+++ bot.py
@@ -1,67 +1,19 @@
-from telegram import Update
-from telegram.ext import Application, CommandHandler, ContextTypes
-from telegram.ext import PicklePersistence
-from esperanto import esperanto
-from enfs import enfs
-from translate import translate
-from mbway import mbway, fetch_prize_table
 import datetime
-
-# from pydub import AudioSegment
-import tempfile
 
 # from urllib.request import urlopen
 # import requests
 import logging
 
+# from pydub import AudioSegment
+
+from telegram import Update
+from telegram.ext import Application, CommandHandler, ContextTypes, PicklePersistence
+
+from enfs import enfs
+from mbway import fetch_prize_table, mbway
+from translate import translate
+
 logging.basicConfig(filename="bot_log.log", encoding="utf-8", level=logging.INFO)
-
-
-def escape_chars(text):
-    reserved_chars = """?&|!{}[]()^~:\\"'+-."""
-
-    mapper = ["\\" + ele for ele in reserved_chars]
-    result_mapping = str.maketrans(dict(zip(reserved_chars, mapper)))
-    return text.translate(result_mapping)
-
-
-# async def send_esperanto(update: Update, context: ContextTypes.DEFAULT_TYPE):
-#     data = esperanto()
-#     month, day, year = data["date"].split("-")
-#     date = "-".join([day, month, year])
-
-#     message = f"""Esperanto Word of the Day ({date}):
-# *{data["word"]}*
-# Word type: {data["wordtype"]}
-# Translation: *{data["translation"]}*
-# Example phrase: {data["fnphrase"]}
-# Example phrase translation: {data["enphrase"]}
-# """
-#     message = escape_chars(message)
-
-#     wordmp3 = urlopen(data["wordsound"]).read()
-#     with tempfile.NamedTemporaryFile(delete=True) as f:
-#         f.write(wordmp3)
-#         AudioSegment.from_mp3(f.name).export("word_audio.ogg", format="ogg")
-
-#     phrasemp3 = urlopen(data["phrasesound"]).read()
-#     with tempfile.NamedTemporaryFile(delete=True) as f:
-#         f.write(phrasemp3)
-#         AudioSegment.from_mp3(f.name).export("phrase_audio.ogg", format="ogg")
-
-#     await context.bot.send_message(
-#         chat_id=update.effective_chat.id, text=message, parse_mode='MarkdownV2')
-
-#     with open("word_audio.ogg", "rb") as f:
-#         await context.bot.send_voice(chat_id=update.effective_chat.id,
-#                                      voice=f,
-#                                      caption="Word pronounciation")
-
-#     with open("phrase_audio.ogg", "rb") as f:
-#         await context.bot.send_voice(chat_id=update.effective_chat.id,
-#                                      voice=f,
-#                                      caption="Phrase pronounciation",
-#                                      filename="phrase")
 
 
 async def send_enfs(update: Update, context: ContextTypes.DEFAULT_TYPE):
@@ -132,6 +84,7 @@ async def send_mbway_notification(context: ContextTypes.DEFAULT_TYPE):
         logging.error(f"Error fetching prize table: {e}")
 
     message += mbway(datetime.datetime.now())
+    logging.info(f"Sending message to {job.chat_id}: {message}")
     await context.bot.send_message(chat_id=job.chat_id, text=message)
 
 
